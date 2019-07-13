7e0150228ec6f4f9e06eb1120410c61966b103ea
Cloe Kouadjo <armelfli50@gmail.com>
2019-07-13 19:23:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,6 +30,12 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
+
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |e| 
+    
+  if array.include?("#{e}") 
+    return array[array.index("#{e}")]
+  end
+end
 end
