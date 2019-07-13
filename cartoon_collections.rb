9430257a0702e85860eedae7a15f97fed4cc184a
Cloe Kouadjo <armelfli50@gmail.com>
2019-07-13 18:59:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
 def roll_call_dwarves(array)
   # Your code here
+  i = 0
+  array.each{|element| puts "#{i} #{element}"}
 end
 
 def summon_captain_planet(array)
@@ -10,7 +12,7 @@ def long_planeteer_calls(array)
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
