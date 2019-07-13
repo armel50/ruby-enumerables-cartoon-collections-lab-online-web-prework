2b7c21c1cc250e62b218a8226568563ee7b2e87e
Cloe Kouadjo <armelfli50@gmail.com>
2019-07-13 19:05:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,8 @@ end
 end
 
 def summon_captain_planet(array)
-  array.map{|element| element.capitalize()}
-
+  newArr = array.map{|element| element.capitalize()}
+  newArr
 def long_planeteer_calls(array)
   # Your code here
 end
